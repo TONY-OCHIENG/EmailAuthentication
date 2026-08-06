01983156eff00cc1e1a62288f178dbe33ac3b538
TONY-OCHIENG <tonyochiengdh@gmail.com>
2026-08-06 18:39:38 +0300

    Initializing express server

---
+++ server/index.js
@@ -0,0 +1,13 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import cookieParser from 'cookie-parser'
+import cors from 'cors'
+dotenv.config()
+
+const app = express()
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors())
+app.listen(process.env.PORT,() => {
+    console.log("server is running")
+})
