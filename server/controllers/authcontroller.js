13c02619d2aa202933693854aafaab6164a9e88e
TONY-OCHIENG <tonyochiengdh@gmail.com>
2026-08-07 07:36:48 +0300

    Validating email using regex

--- server/controllers/authcontroller.js
+++ server/controllers/authcontroller.js
@@ -8,8 +8,16 @@ export const signup = async (request,response) => {
     //checking password length
     if (password.length < 6) {
         return response.status(400).json({
-            status: false, 
+            success: false, 
             message:"Password length must be greater than five characters"
+        })
+    }
+    //validating email using regex
+    const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+    if (!regex.test(email)) {
+        return response.status(400).json({
+            success: false,
+            message:"Enter a valid email"
         })
     }
 }
