ccee010f9602a097c45738c09e15654f08cdf450
TONY-OCHIENG <tonyochiengdh@gmail.com>
2026-08-07 07:24:50 +0300

    Adding route file and controllers file

--- server/index.js
+++ server/index.js
@@ -2,14 +2,14 @@ import express from 'express'
 import dotenv from 'dotenv'
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
-import conn from './database/db.js'
-conn
+import authRoutes from './routes/auth.routes.js'
 dotenv.config()
 
 const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors())
+app.use("/api/auth",authRoutes)
 app.listen(process.env.PORT,() => {
     console.log("server is running")
 })
