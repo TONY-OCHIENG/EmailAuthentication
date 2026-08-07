1e8e5fc6785300f9453c936eac05195341d62e1a
TONY-OCHIENG <tonyochiengdh@gmail.com>
2026-08-07 09:42:05 +0300

    Implementing a function that sends verification code through email

---
+++ server/mail/mail.js
@@ -0,0 +1,28 @@
+import nodemailer from 'nodemailer'
+import dotenv from 'dotenv'
+import { WELCOME_VERIFICATION } from './emailTemplate.js'
+dotenv.config()
+
+export const verificationCodeEmail = async (email,verificationCode) => {
+    try {
+        const transportert = nodemailer.createTransport({
+            host: process.env.HOST,
+            port: process.env.EMAIL_PORT,
+            secure: false,
+            auth: {
+                user: process.env.EMAIL_USER,
+                pass: process.env.EMAIL_PASSWORD
+            }
+        })
+        const message = {
+            to: email,
+            subject: "Verification code",
+            html: WELCOME_VERIFICATION.replace("{VERIFICATION_CODE}}",verificationCode)
+        };
+        console.log("Verification email sent successfully")
+    } catch (error) {
+        console.log(error)
+        throw new Error(error)
+    }
+
+}
