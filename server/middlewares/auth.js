3fbbcf70b18ba1f45fcda1df787c2ec545b3b607
TONY-OCHIENG <tonyochiengdh@gmail.com>
2026-08-08 15:27:47 +0300

    Implementing authentication middleware

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -1,1 +1,20 @@
 import jwt from 'jsonwebtoken'
+import dotenv from 'dotenv'
+dotenv.config()
+
+export const auth = (request,response,next) => {
+    const token = request.cookies.token
+    if (!token) {
+        return response.status(401).json({status: false, message:"You are not loggedin"})
+    } else {
+        jwt.verify(token,process.env.JWT_SECRET,(err,decoded) => {
+            if (err) {
+                return response.status(401).json({status:false, message:"Access denied"})
+            } else {
+                request.lastName = decoded.lastName
+                request.firstName = decoded.firstName
+                next()
+            }
+        })
+    }
+}
