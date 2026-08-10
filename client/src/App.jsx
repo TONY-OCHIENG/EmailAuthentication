d65a2d618835655aae0b71b91a051c77238e1757
TONY-OCHIENG <tonyochiengdh@gmail.com>
2026-08-10 11:48:34 +0300

    Adding routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { Toaster } from 'react-hot-toast'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Signup from './pages/Signup'
+import Code from './pages/Code'
+import Login from './pages/Login'
 
 function App() {
   return (
-    <div>App</div>
+    <div>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/signup' element={<Signup/>}/>
+        <Route path='/verification-code' element={<Code/>}/>
+        <Route path='/login' element={<Login/>}/>
+      </Routes>
+      <Toaster/>
+    </div>
   )
 }
 
