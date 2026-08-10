0fabb15a605e88586e5b3ed382ee83bd0e9d483b
TONY-OCHIENG <tonyochiengdh@gmail.com>
2026-08-10 12:30:01 +0300

    Adding floatingIcons component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,10 +5,15 @@ import Home from './pages/Home'
 import Signup from './pages/Signup'
 import Code from './pages/Code'
 import Login from './pages/Login'
+import FloatingIcons from './components/FloatingIcons'
 
 function App() {
   return (
-    <div>
+    <div className='min-h-screen bg-gradient-to-br from-gray-900 via-gray-800 to-gray-900 overflow-hidden
+    relative flex items-center justify-center'>
+      <FloatingIcons color="bg-gray-600" size="w-64 h-64" top="-5%" left="10%" delay={0}/>
+      <FloatingIcons color="bg-gray-600" size="w-48 h-48" top="70%" left="80%" delay={5}/>
+      <FloatingIcons color="bg-gray-600" size="w-32 h-32" top="40%" left="-10%" delay={2}/>
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/signup' element={<Signup/>}/>
