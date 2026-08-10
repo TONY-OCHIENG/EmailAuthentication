19e93c3ce270ff15dfeddf7e10053b22f7604b6e
TONY-OCHIENG <tonyochiengdh@gmail.com>
2026-08-10 16:19:06 +0300

    Implementing cors

--- server/index.js
+++ server/index.js
@@ -8,7 +8,11 @@ dotenv.config()
 const app = express()
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors())
+app.use(cors({
+    origin:'http://localhost:5173',
+    methods:['PUT','GET','POST','DELETE'],
+    credentials:true,
+}))
 app.use("/api/auth",authRoutes)
 app.listen(process.env.PORT,() => {
     console.log("server is running")
