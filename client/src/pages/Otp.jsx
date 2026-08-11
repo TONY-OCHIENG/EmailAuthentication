23d0af97f80fa03ca0892a871cd00cfbe0bf0415
TONY-OCHIENG <tonyochiengdh@gmail.com>
2026-08-11 10:55:07 +0300

    Refactoring

--- client/src/pages/Otp.jsx
+++ client/src/pages/Otp.jsx
@@ -3,13 +3,16 @@ import { useRef } from 'react'
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { motion } from 'framer-motion'
+import axios from 'axios'
+import toast from 'react-hot-toast'
+const API_URL = import.meta.env.MODE === "development" ? "http://localhost:5000/api/auth" : "/api/auth"
 function Otp() {
-  const [code,setCode] = useState(["","","","","",""])
+  const [verificationCode,setCode] = useState(["","","","","",""])
   const inputRef = useRef([])
+  const [isLoading,setLoading] = useState(false)
   const navigate = useNavigate()
   const handleChange = (index, value) => {
-    const newCode = [...code];
-
+    const newCode = [...verificationCode];
 		// Handle pasted content
 		if (value.length > 1) {
 			const pastedCode = value.slice(0, 6).split("");
@@ -31,12 +34,35 @@ function Otp() {
 			if (value && index < 5) {
 				inputRef.current[index + 1].focus();
 			}
-		}
+	  	}
     }
     const handlekey = (index,e) => {
-        	if (e.key === "Backspace" && !code[index] && index > 0) {
+      if (e.key === "Backspace" && !verificationCode[index] && index > 0) {
 			inputRef.current[index - 1].focus();
-		}
+		 }
+    }
+    const handleSubmit = (event) => {
+      event.preventDefault()
+      const code = verificationCode.join("")
+      console.log(code)
+      setLoading(!isLoading)
+      try {
+        axios.post(`${API_URL}/verifyCode`,{code})
+        .then((response) => {
+
+          if (response.data.success) {
+            setLoading(false)
+            toast.success(response.data.message)
+          }
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+        
+      } catch (error) {
+        console.log(error)
+      }
+
     }
   return (
     <div className='max-w-md bg-gray-800/50  backdrop-filter backdrop-blur-xl rounded-2xl shadow-xl overflow-hidden'>
@@ -49,11 +75,11 @@ function Otp() {
           <h2 className='text-3xl font-bold mb-6 text-center bg-gradient-to-r from-gray-400 to-gray-500 text-transparent bg-clip-text'>
 		       Verify Your Email
 		      </h2>
-		      <p className='text-center text-gray-300 mb-6'>Enter the 6-digit code sent to your email address.</p>
-      <form action="" className='space-y-6'>
+		      <p className='text-center text-gray-300 mb-6'>Enter the 6-digit verification code sent to your email address.</p>
+      <form action="" className='space-y-6' onSubmit={handleSubmit}>
         <div className='flex justify-between'>
           {
-            code.map((digit,index) => (
+            verificationCode.map((digit,index) => (
               <input
               key={index}
               ref={(el) => (inputRef.current[index] = el)}
@@ -67,6 +93,15 @@ function Otp() {
             ))
           }
         </div>
+           <motion.button
+						whileHover={{ scale: 1.05 }}
+						whileTap={{ scale: 0.95 }}
+						type='submit'
+						disabled={isLoading || verificationCode.some((digit) => !digit)}
+						className='w-full bg-gradient-to-r from-gray-500 to-gray-600 text-white font-bold py-3 px-4 rounded-lg shadow-lg hover:from-gray-600 hover:to-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-opacity-50 disabled:opacity-50'
+			>
+						{isLoading ? "Verifying..." : "Verify Email"}
+			</motion.button>
       </form>
       </motion.div>
     </div>
